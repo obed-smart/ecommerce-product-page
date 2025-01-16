730b49b6430c0cb78dab36de5773424c0587ea4a
chukwu obed <codingwithsmart@gmail.com>
2025-01-16 21:08:06 +0100

    modified the cart container to display and update when new the quantity is increased

--- public/index.js
+++ public/index.js
@@ -1,4 +1,5 @@
 const menu = document.getElementById("menu");
+const addButton = document.getElementById("add-cart-btn");
 const openMenu = document.getElementById("open-menu");
 const closeMenu = document.getElementById("close-menu");
 const modal = document.getElementById("modal");
@@ -13,21 +14,30 @@ const slideButton2 = document.querySelectorAll(".slide-btn2");
 const addQuantityButton = document.querySelectorAll(".add-quantity");
 const productQuantity = document.querySelector(".quantity");
 const productPrice = document.querySelector(".product-price");
+const cartButton = document.querySelector(".cart-icon");
+const cartContainer = document.querySelector(".cart");
 
 let quantity = 1;
 let price = 125;
+let cart = {};
 
 let slideIndex = 1;
 updateQuantityAndPrice();
 slideShow(slideIndex);
+cartMessage();
+
+// open mobile menu
 
 openMenu.addEventListener("click", () => {
   menu.classList.add("open");
 });
 
+// close mobile menu
 closeMenu.addEventListener("click", () => {
   menu.classList.remove("open");
 });
+
+// select the index on the active product image
 
 function currentSlide(index) {
   slideShow((slideIndex = index));
@@ -61,8 +71,42 @@ slideButton2.forEach((button, index) => {
 
 function updateQuantityAndPrice() {
   productQuantity.textContent = quantity;
-  productPrice.innerHTML = `$${price * quantity}.00`;
+  productPrice.textContent = price * quantity;
 }
+
+function addToCart(e) {
+  const productId = e.currentTarget.parentNode.parentNode.dataset.id;
+  const productIMage = document.querySelector(".product").src;
+  const productName = document.querySelector(".product-name").textContent;
+  const productCartPrice = price;
+  const productCartQuantity = Number(productQuantity.textContent);
+  const productTotalPrice = productCartPrice * productCartQuantity;
+
+  if (cart[productId]) {
+    cart[productId].productCartQuantity += Number(productQuantity.textContent);
+    cart[productId].productTotalPrice =
+      cart[productId].productCartQuantity * productCartPrice;
+    displayCart();
+  }
+
+  if (!cart[productId]) {
+    cart[productId] = {
+      productIMage,
+      productName,
+      productCartPrice,
+      productCartQuantity,
+      productTotalPrice,
+    };
+  }
+
+  quantity = 1;
+  document.querySelector(".cart-list").classList.remove("empty");
+  updateQuantityAndPrice();
+  displayCart();
+  cartMessage();
+}
+
+// console.log(cart);
 
 addQuantityButton.forEach((button, index) => {
   button.addEventListener("click", () => {
@@ -78,19 +122,92 @@ addQuantityButton.forEach((button, index) => {
   });
 });
 
+// add product to the cart
+addButton.addEventListener("click", (e) => {
+  const productId = e.currentTarget.parentNode.parentNode.dataset.id;
+
+  addToCart(e);
+});
+
+// display cart notification
+
+function cartMessage() {
+  const productMsgQuantity = document.querySelector(".cart-msg");
+  let productNotification = 0;
+  productMsgQuantity.textContent = productNotification;
+
+  for (const product of Object.values(cart)) {
+    productNotification += product.productCartQuantity;
+
+    productMsgQuantity.textContent = productNotification;
+  }
+}
+
+// open and close the product cart
+
+cartButton.addEventListener("click", () => {
+  cartContainer.classList.toggle("show-cart");
+});
+
+// display the product on the cart
+
+function displayCart() {
+  const cartList = document.querySelector(".cart-list");
+  let product;
+  for (const item of Object.values(cart)) {
+    product = `<div data-id="${item.productId}" class=" relative mt-3 lg:mt-2">
+  <div class="flex gap-4 items-center ">
+    <img src="../images/image-product-1.jpg" alt="" class="w-16 aspect-square rounded-lg lg:w-14">
+    <div class="space-y-2">
+      <h1 class="text-[1.1rem] text-darkGrayishBlue font-bold">${item.productName}</h1>
+      <p class="text-xl">
+        <span class="text-darkGrayishBlue">$${item.productCartPrice}.00</span> x
+        <span class="text-darkGrayishBlue">${item.productCartQuantity}</span>
+        <span class="text-veryDarkBlue font-bold">$${item.productTotalPrice}.00</span>
+      </p>
+    </div>
+    <img id="delete-cart-btn" src="../images/icon-delete.svg" alt="" class="absolute cursor-pointer right-0 top-10 w-4">
+  </div>
+  <button id="checkout-btn"   class="w-full lg:mt-4 lg:py-2 lg:text-xl mt-8 text-2xl font-semibold bg-customOrange py-4 rounded-lg">
+    Checkout
+  </button>
+  </div>`;
+    cartList.innerHTML = product;
+    deleteCartproduct(item, cartList);
+  }
+
+  const checkOutButton = document.getElementById("checkout-btn");
+
+  checkOutButton.addEventListener("click", () => {
+    setTimeout(() => {
+      location.reload();
+    }, 1000);
+  });
+}
+
+// slide to the clicked , next or previous product image
+
 function slideShow(index) {
+  // return to the first index when the product reach last
+
   if (index > products.length) slideIndex = 1;
   if (index < 1) slideIndex = products.length;
+
+  // hide the privious index of the product when moving to another
 
   for (const index of Object.keys(products)) {
     products[index].style.display = "none";
     products2[index].style.display = "none";
   }
 
+  // remove the active state on the thumbnails
+
   for (const index of Object.keys(thumbnails)) {
     thumbnails[index].classList.remove("current");
     thumbnails2[index].classList.remove("current");
   }
+
+  // display the first image and thumbnail of the product
 
   products[slideIndex - 1].style.display = "block";
   products2[slideIndex - 1].style.display = "block";
@@ -98,10 +215,36 @@ function slideShow(index) {
   thumbnails2[slideIndex - 1].classList.add("current");
 }
 
-closeModal.addEventListener("click", () => {
-  modal.classList.remove("lg:flex");
-});
+// open modal
 
 openModal.addEventListener("click", () => {
   modal.classList.add("lg:flex");
 });
+
+// close modal
+closeModal.addEventListener("click", () => {
+  modal.classList.remove("lg:flex");
+});
+
+function deleteCartproduct(item, cartList) {
+  const deleteCartProductBtn = document.getElementById("delete-cart-btn");
+  const productsId = item.productId;
+
+  if (!deleteCartProductBtn) return;
+
+  const cartId = Object.keys(cart).find(
+    (item) => cart[item].productId === productsId
+  );
+  if (!cart[cartId]) return;
+  const emptyMg = document.querySelector(".cart-list");
+
+  deleteCartProductBtn.addEventListener("click", (e) => {
+    cartList.innerHTML = `<p class="text-darkGrayishBlue group-[&:not(.empty)]:hidden  text-xl font-semibold">Your cart is empty</p>`;
+    emptyMg.classList.add("empty");
+
+    e.target.parentNode.parentNode.remove();
+    delete cart[cartId];
+
+    cartMessage();
+  });
+}
